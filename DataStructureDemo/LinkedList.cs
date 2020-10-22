eb98979334a78971042798537c62d4a8f74c2251
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-10-23 03:25:03 +0530

    [pranav] Add. Add elements at the start

--- DataStructureDemo/LinkedList.cs
+++ DataStructureDemo/LinkedList.cs
@@ -7,5 +7,16 @@ namespace DataStructureDemo
     class LinkedList
     {
         internal Node head;
+
+        internal void addatBeginning(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                node.next = null;
+            else
+                node.next = this.head;
+            this.head = node;
+            Console.WriteLine("{0} is inserted at the head ", node.data);
+        }
     }
 }
--- DataStructureDemo/Program.cs
+++ DataStructureDemo/Program.cs
@@ -7,7 +7,10 @@ namespace DataStructureDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List application");
-            
+            LinkedList list = new LinkedList();
+            list.addatBeginning(3);
+            list.addatBeginning(43);
+            list.addatBeginning(51);
         }
     }
 }
