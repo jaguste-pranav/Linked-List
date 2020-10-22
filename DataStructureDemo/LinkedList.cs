277aa03f860e1bb7b215c5b8b71bdccf78679e93
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-10-23 03:29:11 +0530

    [pranav] Add. Add elements at the end

--- DataStructureDemo/LinkedList.cs
+++ DataStructureDemo/LinkedList.cs
@@ -18,5 +18,22 @@ namespace DataStructureDemo
             this.head = node;
             Console.WriteLine("{0} is inserted at the head ", node.data);
         }
+
+        public void addAtEnd(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} is inserted into the linked list", node.data);
+        }
     }
 }
--- DataStructureDemo/Program.cs
+++ DataStructureDemo/Program.cs
@@ -8,9 +8,9 @@ namespace DataStructureDemo
         {
             Console.WriteLine("Welcome to Linked List application");
             LinkedList list = new LinkedList();
-            list.addatBeginning(3);
-            list.addatBeginning(43);
-            list.addatBeginning(51);
+            list.addAtEnd(3);
+            list.addAtEnd(43);
+            list.addAtEnd(51);
         }
     }
 }
