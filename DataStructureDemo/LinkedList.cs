1ccd150e3c0a8c39fb2ffd4be483685c14df7654
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-10-23 03:32:41 +0530

    [pranav] Add. Add elements at any position

--- DataStructureDemo/LinkedList.cs
+++ DataStructureDemo/LinkedList.cs
@@ -35,5 +35,49 @@ namespace DataStructureDemo
             }
             Console.WriteLine("{0} is inserted into the linked list", node.data);
         }
+
+        public void insertInGivenPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+                Console.WriteLine("{0} is inserted at position {1}", data, position);
+            }
+            else
+            {
+                Node temp = this.head;
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = temp.next;
+                        temp.next = node;
+                        break;
+                    }
+                    temp = temp.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+        }
+        public void display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + "->");
+                temp = temp.next;
+            }
+        }
     }
 }
--- DataStructureDemo/Program.cs
+++ DataStructureDemo/Program.cs
@@ -11,6 +11,9 @@ namespace DataStructureDemo
             list.addAtEnd(3);
             list.addAtEnd(43);
             list.addAtEnd(51);
+
+            list.insertInGivenPosition(2, 50);
+            list.display();
         }
     }
 }
